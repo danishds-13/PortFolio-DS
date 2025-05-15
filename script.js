dbb5878c7ff617b2e98fe44bd46a881d853924d0
danish_ds <danishdeepasoman02@gmail.com>
2025-05-15 11:56:41 +0530

    Updated files

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+  document.getElementById('characterBox').addEventListener('click', function () {
+    document.querySelector('.about-container').classList.toggle('about-active');
+  });
+</script>
